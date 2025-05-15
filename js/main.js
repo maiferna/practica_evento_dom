4b525a2612a9490c6d831d4f49f7764e222f6018
maiferna <maiderfernandez57@gmail.com>
2025-05-15 13:27:36 +0200

    ADD: css, html y variables js

---
+++ js/main.js
@@ -0,0 +1,62 @@
+
+// Variables
+
+const fragment = document.createDocumentFragment();
+const imgUrl = "assets/img/viajes/";
+
+
+const arrButton = ["Mar", "Montaña", "Ciudad", "Lago"]; // Generar los tags a partir del array arrImages.tags con un método
+
+
+const arrImages = [
+    {
+        titulo: 'Viaje 1',
+        url: `${imgUrl}viajes-1.jpg`,
+        alt: 'Imagen 1',
+        descripcion: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, perspiciatis?',
+        tags: ['mar', 'palmera', 'chica', 'cielo'],
+    },
+    {
+        titulo: 'Viaje 2',
+        url: `${imgUrl}viajes-2.jpg`,
+        alt: 'Imagen 2',
+        descripcion: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, perspiciatis?',
+        tags: ['mar', 'arena', 'puente', 'cielo', 'casas'],
+    },
+    {
+        titulo: 'Viaje 3',
+        url: `${imgUrl}viajes-3.jpg`,
+        alt: 'Imgen 3',
+        descripcion: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, perspiciatis?',
+        tags: ['palmera', 'agua', 'cielo'],
+    },
+    {
+        titulo: 'Viaje 4',
+        url: `${imgUrl}viajes-4.jpg`,
+        alt: 'Imagen 4',
+        descripcion: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, perspiciatis?',
+        tags: ['palmera', 'agua', 'cielo'],
+    },
+    {
+        titulo: 'Viaje 5',
+        url: `${imgUrl}viajes-5.jpg`,
+        alt: 'Imagen 5',
+        descripcion: 'Breve descripción de la imagen 5',
+        tags: ['palmera', 'agua', 'cielo'],
+    },
+    {
+        titulo: 'Viaje 6',
+        url: `${imgUrl}viajes-6.jpg`,
+        alt: 'Imagen 6',
+        descripcion: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, perspiciatis?',
+        tags: ['palmera', 'agua', 'cielo'],
+    },
+    {
+        titulo: 'Viaje 7',
+        url: `${imgUrl}viajes-7.jpg`,
+        alt: 'Imagen 7',
+        descripcion: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, perspiciatis?',
+        tags: ['palmera', 'agua', 'cielo'],
+    }
+]
+
